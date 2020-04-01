42ac24583ee190889cb53fe336af48f88d41900c
tinypy <1306359812@qq.com>
2020-04-01 18:08:08 +0800

    commit

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,4 +1,4 @@
-<style lang='less'>
+<style lang="less">
 .app-home-vue {
   .home-part-body {
     height: 350px;
@@ -41,6 +41,8 @@
     font-size: 28px;
     text-align: center;
   }
+  .lost {
+  }
 }
 </style>
 <template>
@@ -56,7 +58,7 @@
               </el-tooltip>
             </div>
           </div>
-          <div class="count">{{count.userAccount}} 人</div>
+          <div class="count">{{ count.userAccount }} 人</div>
         </el-card>
       </Cell>
       <Cell :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
@@ -69,7 +71,7 @@
               </el-tooltip>
             </div>
           </div>
-          <div class="count">{{count.lostCount}} 件</div>
+          <div class="count">{{ count.lostCount }} 件</div>
         </el-card>
       </Cell>
       <Cell :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
@@ -82,7 +84,7 @@
               </el-tooltip>
             </div>
           </div>
-          <div class="count">{{count.foundCount}} 件</div>
+          <div class="count">{{ count.foundCount }} 件</div>
         </el-card>
       </Cell>
       <Cell :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
@@ -95,7 +97,7 @@
               </el-tooltip>
             </div>
           </div>
-          <div class="count">{{count.newsCount}} 条</div>
+          <div class="count">{{ count.newsCount }} 条</div>
         </el-card>
       </Cell>
       <Cell :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
@@ -108,7 +110,7 @@
               </el-tooltip>
             </div>
           </div>
-          <div class="count">{{count.announceCount}} 条</div>
+          <div class="count">{{ count.announceCount }} 条</div>
         </el-card>
       </Cell>
       <Cell :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
@@ -121,7 +123,7 @@
               </el-tooltip>
             </div>
           </div>
-          <div class="count">{{count.thanksCount}} 条</div>
+          <div class="count">{{ count.thanksCount }} 条</div>
         </el-card>
       </Cell>
     </Row>
@@ -129,12 +131,15 @@
       <Cell :xs="24" :sm="24" :md="24" :lg="16" :xl="16">
         <div class="h-panel shadow">
           <div class="h-panel-bar">
-            <span class="h-panel-title">
-              <Tabs class="common-panel-tabs" v-model="type" :datas="{lost: '寻物启事分类统计', found: '招领启事分类统计'}"></Tabs>
-            </span>
+            <Tabs v-model="type" :datas="{ lost: '寻物启事分类统计', found: '招领启事分类统计' }"></Tabs>
           </div>
           <div class="h-panel-body">
-            <div id="c1"></div>
+            <transition name="el-fade-in">
+              <div v-show="type == 'lost'" class="lost" id="c1"></div>
+            </transition>
+            <transition name="el-fade-in">
+              <div v-show="type == 'found'" class="lost" id="c2"></div>
+            </transition>
           </div>
         </div>
       </Cell>
@@ -142,46 +147,37 @@
       <Cell :xs="24" :sm="24" :md="24" :lg="8" :xl="8">
         <div class="h-panel shadow">
           <div class="h-panel-bar">
-            <div class="h-panel-title">订单统计</div>
-            <div class="h-panel-right">
-              <span class="gray-color">总共达成</span>
-              <i class="h-split"></i>
-              <span class="font20 primary-color">200</span>
-              <i class="h-split"></i>
-              <span class="gray-color">W</span>
-            </div>
+            <div class="h-panel-title">失物统计</div>
           </div>
-          <div class="h-panel-body progress-div home-part-body">
-            <p>
-              <Progress :percent="99" color="green">
-                <span slot="title">订单状态</span>
-                <span slot="text">4个</span>
-              </Progress>
-            </p>
-            <p>
-              <Progress :percent="88" color="blue">
-                <span slot="title">订单状态</span>
-                <span slot="text">0个</span>
-              </Progress>
-            </p>
-            <p>
-              <Progress :percent="55" color="red">
-                <span slot="title">订单状态</span>
-                <span slot="text">0个</span>
-              </Progress>
-            </p>
-            <p>
-              <Progress :percent="77" color="blue">
-                <span slot="title">订单状态</span>
-                <span slot="text">0个</span>
-              </Progress>
-            </p>
-            <p>
-              <Progress :percent="66" color="yellow">
-                <span slot="title">订单状态</span>
-                <span slot="text">0个</span>
-              </Progress>
-            </p>
+          <div class="h-panel-body">
+            <h-circle :percent="parseInt((lostBili.yes / lostBili.count) * 100)" :stroke-width="16" :size="180">
+              <p class="gray-color" v-font="28">总数：{{ lostBili.count }}</p>
+              <div v-height="5"></div>
+              <p class="dark-color" v-font="13">正在寻找/已寻回比例</p>
+              <p class="gray-color">
+                <span class="primary-color" v-font="16">{{ lostBili.no }}</span
+                >/<span>{{ lostBili.yes }}</span>
+              </p>
+            </h-circle>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <h-circle :percent="parseInt((lostBili.yes / lostBili.count) * 100)" :stroke-width="8" :size="80"
+              ><span v-font="12">占比: {{ lostBili.yes }}%</span></h-circle
+            >
+          </div>
+          <div class="h-panel-body">
+            <h-circle :percent="parseInt((foundBili.yes / foundBili.count) * 100)" :stroke-width="16" :size="180">
+              <p class="gray-color" v-font="28">总数：{{ foundBili.count }}</p>
+              <div v-height="5"></div>
+              <p class="dark-color" v-font="13">正在认领/已被认领比例</p>
+              <p class="gray-color">
+                <span class="primary-color" v-font="16">{{ foundBili.no }}</span
+                >/<span>{{ foundBili.yes }}</span>
+              </p>
+            </h-circle>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <h-circle :percent="parseInt((foundBili.yes / foundBili.count) * 100)" :stroke-width="8" :size="80"
+              ><span v-font="12">占比: {{ foundBili.yes }}%</span></h-circle
+            >
           </div>
         </div>
       </Cell>
@@ -194,7 +190,11 @@ export default {
   data() {
     return {
       type: 'lost',
-      count: {}
+      count: {},
+      lostType: [],
+      foundType: [],
+      lostBili: {},
+      foundBili: {}
     };
   },
   methods: {
@@ -207,33 +207,64 @@ export default {
       });
     },
     showLostTypeCount() {
-      const data = [
-        { year: '1951 年', sales: 38 },
-        { year: '1952 年', sales: 52 },
-        { year: '1956 年', sales: 61 },
-        { year: '1957 年', sales: 145 },
-        { year: '1958 年', sales: 48 },
-        { year: '1959 年', sales: 38 },
-        { year: '1960 年', sales: 38 },
-        { year: '1962 年', sales: 38 }
-      ];
-      const chart = new Chart({
-        container: 'c1',
-        autoFit: true,
-        height: 350
+      R.Dashboard.lostTypeCount().then(res => {
+        console.log(res);
+        if (res.ok) {
+          this.lostType = res.body;
+          const chart = new Chart({
+            container: 'c1',
+            forceFit: true,
+            height: 430,
+            padding: [20, 20]
+          });
+
+          console.log(chart);
+
+          chart.source(this.lostType);
+
+          chart.interval().position('type*count');
+
+          chart.render();
+        }
       });
+    },
+    showFoundTypeCount() {
+      R.Dashboard.foundTypeCount().then(res => {
+        console.log(res);
+        if (res.ok) {
+          this.foundType = res.body;
+          const chart = new Chart({
+            container: 'c2',
+            forceFit: true,
+            height: 430,
+            padding: [20, 20]
+          });
 
-      chart.source(data);
+          console.log(chart);
 
-      chart.interval().position('year*sales');
+          chart.source(this.foundType);
 
-      chart.render();
+          chart.interval().position('type*count');
+
+          chart.render();
+        }
+      });
+    },
+    showBili() {
+      R.Dashboard.bili().then(res => {
+        console.log(res);
+        if (res.ok) {
+          this.lostBili = res.body[0];
+          this.foundBili = res.body[1];
+        }
+      });
     }
   },
   mounted() {
     this.showCount();
-
     this.showLostTypeCount();
+    this.showFoundTypeCount();
+    this.showBili();
   }
 };
 </script>
